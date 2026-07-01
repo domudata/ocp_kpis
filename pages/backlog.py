b5c1e6b3ab0862dd52073115ac08442b9f3d5310
domu data <domudata@gmail.com>
2026-07-01 17:22:21 +0100

    Delete pages directory

--- pages/backlog.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from components.charts import show_simple_pie
-def render(kpi_data, df, posts):
-    pc_f = kpi_data['pc'][["CARACTERISE","NON CARACTERISE"]].loc[kpi_data['pc'].index.isin(posts)]
-    plc_f = kpi_data['plc'][["CARACTERISE","NON CARACTERISE"]].loc[kpi_data['plc'].index.isin(posts)]
-    c1, c2 = st.columns(2)
-    with c1: show_simple_pie(pc_f, "Backlog Préparation", keep_non_carac=True)
-    with c2: show_simple_pie(plc_f, "Backlog Planification", keep_non_carac=True)
