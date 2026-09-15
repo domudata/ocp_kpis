5856a926e2c639fcb27067dd4c4022245e16176a
domu data <domudata@gmail.com>
2026-09-16 00:48:08 +0100

    Add optional parameter to render_backlog_page

--- pages/backlog.py
+++ pages/backlog.py
@@ -5,5 +5,6 @@ import pandas as pd
 from components.backlog_widgets import render_backlog_tab
 
 
-def render_backlog_page(dfp: pd.DataFrame, vp: list) -> None:
-    render_backlog_tab(dfp, vp)
+def render_backlog_page(dfp: pd.DataFrame, vp: list, df_toutes_dates: pd.DataFrame = None) -> None:
+    render_backlog_tab(dfp, vp, df_toutes_dates=df_toutes_dates)
+
