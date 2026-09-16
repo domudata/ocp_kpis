922488aff5f53fbdb07e099d01fbe522456039b8
domu data <domudata@gmail.com>
2026-09-16 04:01:18 +0100

    Add blank line after render_backlog_page function

--- pages/backlog.py
+++ pages/backlog.py
@@ -9,3 +9,4 @@ def render_backlog_page(dfp: pd.DataFrame, vp: list, *args, **kwargs) -> None:
     df_toutes_dates = kwargs.get('df_toutes_dates', args[0] if len(args) > 0 else None)
     render_backlog_tab(dfp, vp, df_toutes_dates=df_toutes_dates)
 
+
