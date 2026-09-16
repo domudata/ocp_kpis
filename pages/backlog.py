02cbaed650f36d363e6bf0f882433f8fa76c9642
domu data <domudata@gmail.com>
2026-09-16 01:44:36 +0100

    Modify render_backlog_page to accept kwargs and args

--- pages/backlog.py
+++ pages/backlog.py
@@ -5,6 +5,7 @@ import pandas as pd
 from components.backlog_widgets import render_backlog_tab
 
 
-def render_backlog_page(dfp: pd.DataFrame, vp: list, df_toutes_dates: pd.DataFrame = None) -> None:
+def render_backlog_page(dfp: pd.DataFrame, vp: list, *args, **kwargs) -> None:
+    df_toutes_dates = kwargs.get('df_toutes_dates', args[0] if len(args) > 0 else None)
     render_backlog_tab(dfp, vp, df_toutes_dates=df_toutes_dates)
 
