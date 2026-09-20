5f50cfc0464f0cd01039905af2bf1019124272a1
domu data <domudata@gmail.com>
2026-09-20 02:08:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -521,10 +521,7 @@ def main() -> None:
         ])
 
         with tabs[0]:
-            render_dashboard_tab(
-                vp, pscores, qscores, pa, qa, hist_df, now_ts, ano_map,
-                ckdf=ckdf, sf1_p=sf1_p, sf1_q=sf1_q, sf2_p=sf2_p, sf2_q=sf2_q,
-            )
+            render_dashboard_tab(vp, pscores, qscores, pa, qa, hist_df, now_ts, ano_map)
         with tabs[1]:
             render_performance_qualite_tab(vp, ckdf, ano_map, anomaly_dfs, nd_full)
         with tabs[2]:
