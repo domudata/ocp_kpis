7044ac3b02097e5f7ac2a3274756b45a430b8b82
domu data <domudata@gmail.com>
2026-09-26 16:16:10 +0100

    Update backlog.py

--- pages/backlog.py
+++ pages/backlog.py
@@ -7,6 +7,5 @@ from components.backlog_widgets import render_backlog_tab
 
 def render_backlog_page(dfp: pd.DataFrame, vp: list, *args, **kwargs) -> None:
     df_toutes_dates = kwargs.get('df_toutes_dates', args[0] if len(args) > 0 else None)
-    render_backlog_tab(dfp, vp, df_toutes_dates=df_toutes_dates)
-
-
+    hist_df = kwargs.get('hist_df', args[1] if len(args) > 1 else None)
+    render_backlog_tab(dfp, vp, df_toutes_dates=df_toutes_dates, hist_df=hist_df)
